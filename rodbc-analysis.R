3b2b760397351d2d956b995731345bddb32d6cd4
Erik Westlund <ewestlund@jhu.edu>
2024-01-23 09:25:53 -0500

    improvements

--- rodbc-analysis.R
+++ rodbc-analysis.R
@@ -2,10 +2,11 @@
 ## RODBC-based Analysis in R ----------------------------------------------- ##
 
 library(RODBC)
-# library(ggplot2)
-# library(dplyr)
+library(ggplot2)
+library(dplyr)
+library(forcats)
 library(pscl)
-# library(MASS)
+library(MASS)
 library(bench)
 
 # Establish Database Connection--------------------------------------------
@@ -60,8 +61,27 @@ data <- RODBC::sqlFetch(
 # Race: reference = 4 (White Other)
 data <- within(data, raceth <- relevel(factor(raceth), ref = 3))
 
+data$raceth <- fct_recode(
+  data$raceth,
+  "African American" = "1",
+  "Hispanic" = "2",
+  "Middle Eastern" = "3",
+  "White (Other)" = "4",
+  "White (Jewish)" = "5",
+  "Asian" = "6",
+  "Native American/Pacific Islander" = "7"
+)
+
 # Gender: reference = 1 (M)
 data <- within(data, genderbinary <- relevel(factor(genderbinary), ref = 1)) # No change
+
+# Pick appropriate labels basedd upon actual data
+data$genderbinary <- fct_recode(
+  data$genderbinary,
+  "M" = "1",
+  "Not-M" = "0"
+)
+
 
 # Income: reference = 75-99.99k (2)
 # This is a difficult one because the categories are ordered but last category is
@@ -70,14 +90,81 @@ data <- within(data, genderbinary <- relevel(factor(genderbinary), ref = 1)) # N
 # allow "simple" interpetation of having a (preferably) logged income variable.
 data <- within(data, incomecat <- relevel(factor(incomecat), ref = 3)) 
 
+data$incomecat <- fct_recode(
+  data$incomecat,
+  "< 25k" = "1",
+  "25k-50k" = "2",
+  "50k-75k" = "3",
+  "75k-100k" = "4",
+  "100k-125k" = "5",
+  "125-150k" = "6",
+  "> 150k" = "7"
+)
+
+
 # Education: reference = 3 (BA)
 data <- within(data, educationcat <- relevel(factor(educationcat), ref = 3)) 
+
+# Confirm these labels
+data$educationcat <- fct_recode(
+  data$educationcat,
+  "Less than HS" = "1",
+  "HS" = "2",
+  "BA" = "3",
+  "Advanced" = "4"
+)
 
 # Age: reference = 4 (36-45)
 # Like with income, the cateogries are ordered but the boundaries are
 # uncapped.
 data <- within(data, agecat <- relevel(factor(agecat), ref = 4)) 
 
+# Confirm these are correctly linked up
+data$agecat <- fct_recode(
+  data$agecat,
+  "18-24" = "6",
+  "25-34" = "5",
+  "35-44" = "4",
+  "45-54" = "3",
+  "55-64" = "2",
+  ">65" = "1"
+)
+
+# ANOVA & T-tests ---------------------------------------------------------
+
+# Below are some examples of running t-tests in R. The variable on the left-hand
+# side is the dependent variable and the variable on the right-hand side is the
+# independent variable. 
+# 
+# Before running, I think it's useful to do some plotting. I take a sub sample
+# here to make things easier to see. 
+ggplot(data |> sample_n(5000)) +
+  aes(x = genderbinary, y = amvsum, color = genderbinary) +
+  geom_jitter() +
+  theme(legend.position = "none")
+
+t.test(amvsum ~ genderbinary, data = data)
+
+# Of note: a bivariate regressoin yields same difference in means and similar
+# p-value (slightly different degrees of freedom, but trivial here. They are 
+# identical if you you use pooled variance.)
+lm(amvsum ~ genderbinary, data=data) |> summary()
+
+# And ANOVA. Let's first plot:
+# 
+ggplot(data |> sample_n(70000)) +
+  aes(x = raceth, y = amvsum, color = raceth) +
+  geom_jitter() +
+  theme(legend.position = "none")
+
+ggplot(data |> sample_n(70000)) +
+  aes(x = educationcat, y = amvsum, color = educationcat) +
+  geom_jitter() +
+  theme(legend.position = "none")
+
+aov(amvsum ~ raceth, data=data) |> summary()
+aov(amvsum ~ educationcat, data=data) |> summary()
+aov(amvsum ~ incomecat, data=data) |> summary()
 
 # Modeling ----------------------------------------------------------------
 
@@ -86,19 +173,22 @@ data <- within(data, agecat <- relevel(factor(agecat), ref = 4))
 #        imports them.
 
 # Laptop (12-core M3 Pro Macbook): Takes 4.5 minutes to run (3,720,000 records)
-bench::mark(
-  model1 <- glm(
-    amvsum ~ raceth + genderbinary + incomecat + educationcat + agecat,
-    family = poisson(link = "log"), 
-    data = data
-  )
+model1 <- glm(
+  amvsum ~ raceth + genderbinary + incomecat + educationcat + agecat,
+  family = poisson(link = "log"), 
+  data = data
 )
+
 summary(model1)
 
+# This will check for overdispersion and zero-inflation, which may be present
+# and would justify using negative binomial poisson regression and/or a zero-
+# inflation model.
 performance::check_overdispersion(model1)
 performance::check_zeroinflation(model1)
 
 exp(model1$coefficients)
+
 
 # Zero-Inflated Negative Binomial Poisson Regression
 
@@ -108,29 +198,54 @@ exp(model1$coefficients)
 
 # This model has no interactions.
 # Laptop (12-core M3 Pro Macbook): Takes 4.5 minutes to run (3,720,000 records)
-bench::mark(
-  model1_zif <- zeroinfl(
-    amvsum ~ 
-      raceth + genderbinary + incomecat + educationcat + agecat | # Count Model
-      raceth + genderbinary + incomecat + educationcat + agecat, # Zero-Inflation Logit Model
-    data=data, 
-    dist="negbin",
-    link="logit"
-  )
+model1_zif <- zeroinfl(
+  amvsum ~ 
+    raceth + genderbinary + incomecat + educationcat + agecat | # Count Model
+    raceth + genderbinary + incomecat + educationcat + agecat, # Zero-Inflation Logit Model
+  data=data, 
+  dist="negbin",
+  link="logit"
 )
+
 
 summary(model1_zif)
 
+# Running separate logit and neg bin to avoid memory/speed issues
+
+# Binomial with logit link
+# Need to create a new variable for "has visit"/"no visit" -- zero-inflation
+# looks at no visit as outcome (0)
+data <- data |> mutate(
+  no_visit = amvsum < 1,
+  has_visit = amvsum > 0
+)
+
+model1_logit <- glm(
+  no_visit ~ raceth + genderbinary + incomecat + educationcat + agecat,
+  family = binomial(link = "logit"), 
+  data = data
+)
+
+summary(model1_logit) # same as zeroinfl function
+
+# Negative Binomial with log link -- almost identical to zeoinfl function
+model1_negbin <- glm.nb(
+  amvsum ~ raceth + genderbinary + incomecat + educationcat + agecat,
+  data = data |> filter(has_visit),
+  link = "log"
+)
+
+summary(model1_negbin)
+
+
 # This model has interaction of race and income.
-bench::mark(
-  model2_zif <- zeroinfl(
-    amvsum ~ 
-      raceth*incomecat + genderbinary + educationcat + agecat | # Count Model
-      raceth*incomecat + genderbinary + educationcat + agecat, # Zero-Inflation Logit Model
-    data=data, 
-    dist="negbin",
-    link="logit"
-  )
+model2_zif <- zeroinfl(
+  amvsum ~ 
+    raceth*incomecat + genderbinary + educationcat + agecat | # Count Model
+    raceth*incomecat + genderbinary + educationcat + agecat, # Zero-Inflation Logit Model
+  data=data, 
+  dist="negbin",
+  link="logit"
 )
 
 summary(model2_zif)
